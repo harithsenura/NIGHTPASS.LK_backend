a2dc0a148aec8cd8cf83b6d028d6749bcd7ab083
Harith Divarathna <harithdivarathna@gmail.com>
2026-03-25 15:33:41 +0530

    update somethings

--- routes/eventRoutes.js
+++ routes/eventRoutes.js
@@ -1,12 +1,10 @@
 const express = require('express');
 const router = express.Router();
-const { getEvents, getEventById, getEventFullDetails, createEvent, updateEvent, getAdminOverview } = require('../controllers/eventController');
+const { getEvents, getEventById, createEvent, updateEvent, getAdminOverview } = require('../controllers/eventController');
 
 router.get('/', getEvents);
 router.get('/admin/overview', getAdminOverview);
-router.get('/:id/full', getEventFullDetails);
 router.get('/:id', getEventById);
-
 router.post('/', createEvent);
 router.put('/:id', updateEvent);
 
